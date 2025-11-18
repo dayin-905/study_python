47790fea335bcf3e0d1492771000c88ff8631dc9
Lia <dayin.9905@gmail.com>
2025-11-18 17:26:42 +0900

    05

--- 05_ifs.py
+++ 05_ifs.py
@@ -1,17 +1,41 @@
-first = 4
+# if condition:
+#     executed code
+# elif condition2:
+#     executed code
+# else:
+#     executed code  
 
-# if first >= 5 :
+# first = 4
+
+# # if first >= 5 :
+# #     print("Welcome!")
+# #     print("It's me.")
+
+# second = 6
+
+# first = int(input("input first number : "))
+# second = int(input("input second number : "))
+
+# if first >= second :
 #     print("Welcome!")
 #     print("It's me.")
 
-second = 6
 
-first = int(input("input first number : "))
-second = int(input("input second number : "))
+# print("End of program.")
 
-if first >= second :
-    print("Welcome!")
-    print("It's me.")
-
+## 다중 if-elif-else 예제
+age = int(input("input your age : "))
+if age < 4 :
+    print("Your ticket is free!")
+elif age < 18 :
+    print("Your ticket is $5.")
+else:
+    print("Your ticket is $10.")
 
 print("End of program.")
+
+pass
+fourth = "welcome to python!"
+forth_world = "python"
+if fourth in forth_world :
+    print("Yes! 'python' is here.")
