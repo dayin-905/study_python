1f3167336ab38a4b6e3a4cf62b01cadcf1c99299
Lia <dayin.9905@gmail.com>
2025-11-18 10:09:33 +0900

    first commit

---
+++ 05_ifs.py
@@ -0,0 +1,17 @@
+first = 4
+
+# if first >= 5 :
+#     print("Welcome!")
+#     print("It's me.")
+
+second = 6
+
+first = int(input("input first number : "))
+second = int(input("input second number : "))
+
+if first >= second :
+    print("Welcome!")
+    print("It's me.")
+
+
+print("End of program.")
