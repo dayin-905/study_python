3efdc3c66949f623df19a8acca29bf099418c226
Lia <dayin.9905@gmail.com>
2025-11-21 12:56:46 +0900

    00

--- codes/09_classes_calls.py
+++ codes/09_classes_calls.py
@@ -2,7 +2,7 @@ from codes.classes_fourcals import FourCal # FourCal 클래스를 불러오기
 
 # 클래스를 가져옴.
 if __name__ == "__main__":
-    forucal = FourCal() 
+    forucal = FourCal() # 원본 복제
     print(f"3 + 5 = {forucal.add(3, 5)}") 
     print(f"10 + 4 = {forucal.sub(10, 4)}")
     print(f"2 + 6 = {forucal.mul(2, 6)}")
